917b9b3e60e91ce1a29a8b0cb48833b60097c64c
João Pedro <j0310pedro@gmail.com>
2022-01-15 17:39:19 -0300

    e

--- principal/js/menu.js
+++ principal/js/menu.js
@@ -3,9 +3,10 @@ const btnMobile = document.querySelector('.header_nav_btn');
 function toggleMenu(event) {
     if (event.type === 'touchstart') event.preventDefault();
     const nav = document.querySelector('.header_nav');
-    const header = document.querySelector('.header')
-    header.classList.toggle('active')
-    header.classList.toggle('not_active')
+    const header = document.querySelector('.header');
+
+    header.classList.toggle('active');
+    header.classList.toggle('not_active');
     nav.classList.toggle('active');
     nav.classList.toggle('not_active');
     const active = nav.classList.contains('active');
