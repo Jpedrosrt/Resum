91238dfac43233adab6d51f7524bb68cffbee914
João Pedro <j0310pedro@gmail.com>
2022-01-14 22:42:09 -0300

    update

--- principal/js/header.js
+++ principal/js/header.js
@@ -1,9 +1,13 @@
 const header = window.document.querySelector('.header');
 
-window.addEventListener('scroll', function() {
-    if (window.scrollY > 100) {
-        header.classList.add('fixed'); // > 0 ou outro valor desejado
-    } else {
-        header.classList.remove('fixed');
-    }
+window.addEventListener('load', () => {
+    header.classList.remove('fixed');
+    window.addEventListener('scroll', () => {
+        
+        if (window.scrollY > 200) {
+            header.classList.add('fixed'); // > 0 ou outro valor desejado
+        } else {
+            header.classList.remove('fixed');
+        }
+    });
 });
