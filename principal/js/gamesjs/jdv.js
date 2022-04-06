d5e65f8088c0b9ea55912b706285688a19c96f61
João Pedro <j0310pedro@gmail.com>
2022-04-06 15:00:43 -0300

    cm

---
+++ principal/js/gamesjs/jdv.js
@@ -0,0 +1,10 @@
+const box = window.document.querySelectorAll('.box')
+
+const paragrafo = document.createElement("p");
+const jgd1 = document.createTextNode("X");
+const jgd2 = document.createTextNode("O");
+paragrafo.appendChild(jgd1);
+
+for (var i = 0; i < box.length; i++) {
+    box[i].addEventListener("click", (e) => e.target.appendChild(paragrafo))
+}
