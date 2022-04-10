381944cc98b6c772fead4ef6c8115a03aa81d88d
João Pedro <j0310pedro@gmail.com>
2022-04-10 12:10:27 -0300

    eae

--- principal/js/gamesjs/jdv.js
+++ principal/js/gamesjs/jdv.js
@@ -2,6 +2,7 @@ const btn = window.document.querySelectorAll('.btn')
 const turn = window.document.querySelector('.turn')
 const player = window.document.querySelector('.player')
 const pc = window.document.querySelector('.pc')
+var aux = 0
 
 for (var i = 0; i < btn.length; i++) {
     
@@ -20,28 +21,41 @@ for (var i = 0; i < btn.length; i++) {
                 pc.append('X')
                 pc.style.color = "#ff96ad"
             }
+            aux = 1
+            game(aux)
         } else { 
             e.target.className += "op"
             e.path[1].className += "escon"
             turn.classList.remove("escon")
             player.append(e.target.innerHTML)
+            aux = 1
+            game(aux)
         }
     })
 }
 
 var refre = document.querySelector("#refre")
 refre.addEventListener("click", (e) => location.reload())
-
-const op = window.document.querySelectorAll('.btn')
-
-for(var r = 0; r < 2; r++) {
-    console.log(op[r].classList)
-    if(op[r].classList.contains('op')) {
-        console.log('eae')
+function game(aux) {
+    console.log(aux)
+    if (aux == 1) {
+        const op = window.document.querySelector('.op')
+        
         const box = window.document.querySelectorAll('.box')
-
+            
         for (var i = 0; i < box.length; i++) {
-            box[i].addEventListener("click", (e) => e.target.append('oi'))
+            box[i].addEventListener("click", (e) => {
+                
+                var p = document.createElement("p")
+                p.append(op.innerHTML)
+                e.target.append(p)
+                if (op.innerHTML == 'X') {
+                    e.target.style.color = "#ff96ad"
+                } else {
+                    e.target.style.color = "#5CE672"
+                }
+            })
         }
+        
     }
 }
