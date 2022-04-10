439ae31612764f7c296641a20d2793a70c876f38
João Pedro <j0310pedro@gmail.com>
2022-04-09 22:53:44 -0300

    upeae

--- principal/js/gamesjs/jdv.js
+++ principal/js/gamesjs/jdv.js
@@ -1,18 +1,47 @@
 const btn = window.document.querySelectorAll('.btn')
+const turn = window.document.querySelector('.turn')
+const player = window.document.querySelector('.player')
+const pc = window.document.querySelector('.pc')
+
 for (var i = 0; i < btn.length; i++) {
     
-    btn[i].addEventListener('click', (e) => (e.target.classList)? e.target.classList.add("op") : e.target.className += "op")
+    btn[i].addEventListener('click', (e) => {
+        if (e.target.classList) {
+            e.target.classList.add("op")
+            e.path[1].classList.add("escon")
+            turn.classList.remove("escon")
+            player.append(e.target.innerHTML)
+            if (e.target.innerHTML == 'X') {
+                player.style.color = "#ff96ad"
+                pc.append('O')
+                pc.style.color = "#5CE672"
+            } else {
+                player.style.color = "#5CE672"
+                pc.append('X')
+                pc.style.color = "#ff96ad"
+            }
+        } else { 
+            e.target.className += "op"
+            e.path[1].className += "escon"
+            turn.classList.remove("escon")
+            player.append(e.target.innerHTML)
+        }
+    })
 }
 
+var refre = document.querySelector("#refre")
+refre.addEventListener("click", (e) => location.reload())
 
+const op = window.document.querySelectorAll('.btn')
 
-const box = window.document.querySelectorAll('.box')
+for(var r = 0; r < 2; r++) {
+    console.log(op[r].classList)
+    if(op[r].classList.contains('op')) {
+        console.log('eae')
+        const box = window.document.querySelectorAll('.box')
 
-const paragrafo = document.createElement("p");
-const jgd1 = document.createTextNode("X");
-const jgd2 = document.createTextNode("O");
-paragrafo.appendChild(jgd1);
-
-for (var i = 0; i < box.length; i++) {
-    box[i].addEventListener("click", (e) => e.target.appendChild(paragrafo))
+        for (var i = 0; i < box.length; i++) {
+            box[i].addEventListener("click", (e) => e.target.append('oi'))
+        }
+    }
 }
