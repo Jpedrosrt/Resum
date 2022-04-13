839c7be5dcc6ae8e34c4e30f83cf8bfe4efe914c
João Pedro <j0310pedro@gmail.com>
2022-04-13 15:34:45 -0300

    up

--- principal/js/gamesjs/jdv.js
+++ principal/js/gamesjs/jdv.js
@@ -2,7 +2,7 @@ const btn = window.document.querySelectorAll('.btn')
 const turn = window.document.querySelector('.turn')
 const player = window.document.querySelector('.player')
 const pc = window.document.querySelector('.pc')
-var aux = 0
+var aux2 = ['']
 
 for (var i = 0; i < btn.length; i++) {
     
@@ -35,34 +35,56 @@ for (var i = 0; i < btn.length; i++) {
 var refre = document.querySelector("#refre")
 refre.addEventListener("click", (e) => location.reload())
 
+function colc(aux, e, aux2) {
+    let p = document.createElement("p")
+    p.append(aux)
+    e.append(p)
+    aux2.push(e.id)
+    if (aux == 'X') {
+        e.style.color = "#ff96ad"
+    } else {
+        e.style.color = "#5CE672"
+    }
+}
+
 function game(aux) {
     if (aux == 'X' || aux == 'O') {
         
         const box = window.document.querySelectorAll('.box')  
-        let aux2 = ['']
 
         for (var i = 0; i < box.length; i++) {
             box[i].addEventListener("click", (e) => {
-                console.log(aux2)
-                console.log(aux2.includes(e.target.id))
                 if(!aux2.includes(e.target.id)) {
-                    let p = document.createElement("p")
-                    p.append(aux)
-                    e.target.append(p)
-                    aux2.push(e.target.id)
-                    console.log(aux2)
-                    if (aux == 'X') {
-                        e.target.style.color = "#ff96ad"
-                    } else {
-                        e.target.style.color = "#5CE672"
-                    }
+                    
+                    let tar = e.target
+                    colc(aux, tar, aux2)
                 }
+                pcturn(aux, aux2)
             })
         }
-        pc(aux)
+        
     }
 }
 
-function pc(aux) {
-    
+function pcturn(aux, aux2) {
+    const boxs = window.document.querySelectorAll('.box')
+
+    if (aux == 'X') {
+        aux = 'O'
+    } else {
+        aux = 'X'
+    }
+    let a = aux2.map((x) => Number(x[1]))
+    console.log(a)
+    let b = Math.round(Math.random() * 8 ) + 1
+    while(a.includes(b)) {
+        b = Math.round(Math.random()*100) % 8 + 1
+    }
+    console.log(b)
+    console.log(boxs[b])
+    aux2.push()
+    colc(aux, boxs[b], aux2)
+
+    //console.log()
+
 }
