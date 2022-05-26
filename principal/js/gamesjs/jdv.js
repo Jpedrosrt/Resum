1fcd3d269dfbf3b7df0016f5b0c00c940c48820a
João Pedro <j0310pedro@gmail.com>
2022-05-26 00:02:40 -0300

    #

--- principal/js/gamesjs/jdv.js
+++ principal/js/gamesjs/jdv.js
@@ -1,91 +1,125 @@
-const btn = window.document.querySelectorAll('.btn')
-const turn = window.document.querySelector('.turn')
-const player = window.document.querySelector('.player')
-const pc = window.document.querySelector('.pc')
-var aux2 = ['']
+const op1 = document.querySelector('.op1')
+const op2 = document.querySelector('.op2')
+const player = document.querySelector('.player')
+const pc = document.querySelector('.pc')
+const player2 = document.querySelector('.player2')
+const turn = document.querySelector('.turn')
+const boxs = document.querySelectorAll("[data-box]")
+const refs = document.querySelectorAll("[data-refre]")
+const end = document.querySelector('.endgame')
 
-for (var i = 0; i < btn.length; i++) {
-    
-    btn[i].addEventListener('click', (e) => {
-        if (e.target.classList) {
-            e.target.classList.add("op")
-            e.path[1].classList.add("escon")
-            turn.classList.remove("escon")
-            player.append(e.target.innerHTML)
-            if (e.target.innerHTML == 'X') {
-                player.style.color = "#ff96ad"
-                pc.append('O')
-                pc.style.color = "#5CE672"
-            } else {
-                player.style.color = "#5CE672"
-                pc.append('X')
-                pc.style.color = "#ff96ad"
-            }
-            game(e.target.innerHTML)
-        } else { 
-            e.target.className += "op"
-            e.path[1].className += "escon"
-            turn.classList.remove("escon")
-            player.append(e.target.innerHTML)
-            game(e.target.innerHTML)
+
+const wins = [
+    [0, 1, 2], [3, 4, 5],
+    [6, 7, 8], [0, 3, 6],
+    [1, 4, 7], [2, 5, 8],
+    [0, 4, 8], [2, 4, 6],
+]
+
+let plX = []
+let plO = []
+
+for (const refre of refs) {
+    refre.addEventListener("click", (e) => location.reload())
+}
+
+
+let versuPlayer = false;
+let xTurn = turn;
+let optC = false;
+
+const turnM = (x) => (o) => {
+    x.append('X')
+    x.style.color = "#ff96ad"
+    o.append('O')
+    o.style.color = "#5CE672"
+    optC = true
+}
+
+const gameChoice = (e) => {
+    if (e.target.classList[1] == 'btn-2') {
+        versuPlayer = true;
+    }
+    op1.classList.add('escon')
+    op2.classList.remove('escon')
+}
+
+const playerChoice = (e) => {
+    op2.classList.add('escon')
+    turn.classList.remove('escon')
+    op2.parentElement.classList.add('escon')
+    if (versuPlayer) {
+        player2.classList.remove('escon')
+        pc.classList.add('escon')
+        if(e.target.innerHTML == 'X') {
+            turnM(player)(player2)
+        } else {
+            turnM(player2)(player)
         }
+    } else {
+        if(e.target.innerHTML == 'X') {
+            turnM(player)(pc)
+        } else {
+            turnM(pc)(player)
+        }
+    }
+}
+
+
+const willWin = (e) => (mark)=> {
+    if (mark == 'X') plX.push(Number(e.id[1]))
+    else plO.push(Number(e.id[1]))
+    return wins.some(x => {
+        return x.every(y => {
+            if(plX.includes(y) && mark == 'X') return true
+            else if (plO.includes(y) && mark == 'O') return true
+            else return false
+        })
     })
 }
 
-var refre = document.querySelector("#refre")
-refre.addEventListener("click", (e) => location.reload())
+const changeTurn = () => {
+    xTurn = !xTurn
+}
 
-function colc(aux, e, aux2) {
+const addMark = (e) => (mark) => {
     let p = document.createElement("p")
-    p.append(aux)
+    p.append(mark)
     e.append(p)
-    aux2.push(e.id)
-    if (aux == 'X') {
+    if (mark == 'X') {
         e.style.color = "#ff96ad"
     } else {
         e.style.color = "#5CE672"
     }
 }
 
-function game(aux) {
-    if (aux == 'X' || aux == 'O') {
-        
-        const box = window.document.querySelectorAll('.box')  
-
-        for (var i = 0; i < box.length; i++) {
-            box[i].addEventListener("click", (e) => {
-                if(!aux2.includes(e.target.id)) {
-                    
-                    let tar = e.target
-                    colc(aux, tar, aux2)
-                }
-                pcturn(aux, aux2)
-            })
+function coloc(e) {
+    if (optC) {
+        const mark = xTurn ? 'X' : 'O'
+        if(versuPlayer) {
+            addMark(e.target)(mark)
+            changeTurn()
+            if( willWin(e.target)(mark) ) msgEnd(mark)
         }
-        
+        e.target.removeEventListener('click', coloc);
     }
 }
 
-function pcturn(aux, aux2) {
-    const boxs = window.document.querySelectorAll('.box')
-
-    if (aux == 'X') {
-        aux = 'O'
+const msgEnd = (mark) => {
+    end.classList.remove('escon')
+    if (mark == 'X') {
+        end.childNodes[1].innerText = "Winner: X"
+    } else if (mark == 'O') {
+        end.childNodes[1].innerText = "Winner: O"
     } else {
-        aux = 'X'
+        end.childNodes[1].innerText = "Draw!!"
     }
-    let a = aux2.map((x) => Number(x[1]))
-    console.log(a)
-    let b
-    while(b == undefined) {
-        b = Math.floor(Math.random() * 8 ) + 1
-        if (a.includes(b)) {
-            b = undefined
-        }
-    }
-    aux2.push()
-    colc(aux, boxs[b], aux2)
-
-    //console.log()
-
 }
+
+op1.addEventListener("click", gameChoice, { once: true})
+op2.addEventListener("click", playerChoice, { once: true})
+
+for (const box of boxs) {
+    box.addEventListener('click', coloc)
+}
+
